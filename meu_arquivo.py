81a5df030bd5ad58616eef04c362bf2874a89047
David Medeiros <davidmedeiros94@hotmail.com>
2026-05-01 09:18:21 -0300

    Importado e visualizado a planilha corretamente

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -1,7 +1,10 @@
 # Importar a base de dados
 import pandas as pd
+tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
 
 # Faturamento da loja
 
