ff98e00fe4bf42dc12dec37e2a6818208daafa65
David Medeiros <davidmedeiros94@hotmail.com>
2026-05-02 18:45:06 -0300

    Criado a tabela de faturamento, quantidade e ticket médio

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -4,12 +4,22 @@ tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
 pd.set_option('display.max_columns', None)
-print(tabela_vendas)
 
 # Faturamento da loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+print('-' * 50)
 
 # Quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+print('-' * 50)
 
 # Ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame(name='Ticket Médio')
+ticket_medio.columns = ['Ticket Médio']
+print(ticket_medio)
 
 # Enviar um e-mail com o relatório
