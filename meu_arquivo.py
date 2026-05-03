bd36f172d8ae2f1ff00d951afd684c9fe5ac7f17
David Medeiros <davidmedeiros94@hotmail.com>
2026-05-02 22:35:27 -0300

    Configurado o envio e o corpo do e-mail

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -1,5 +1,7 @@
+import pandas as pd 
+import win32com.client as win32
+
 # Importar a base de dados
-import pandas as pd
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
@@ -7,19 +9,39 @@ pd.set_option('display.max_columns', None)
 
 # Faturamento da loja
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-print(faturamento)
-
-print('-' * 50)
 
 # Quantidade de produtos vendidos por loja
 quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-print(quantidade)
-
-print('-' * 50)
 
 # Ticket médio por produto em cada loja
-ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame(name='Ticket Médio')
-ticket_medio.columns = ['Ticket Médio']
-print(ticket_medio)
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
 
 # Enviar um e-mail com o relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'dwmedeiros1994@gmail.com'
+mail.Subject = 'Relatório de vendas por loja'
+mail.HTMLBody = f'''
+<p>Prezados,</p
+
+<p>Segue o relatório de vendas por cada loja.</p
+
+<p>Faturamento:<p/>
+{faturamento.to_html()}
+
+<p>Quantidade vendida:</p>
+{quantidade.to_html()}
+
+<p>Ticket médio dos produtos em cada loja:</p>
+{ticket_medio.to_html()}
+
+<p>Qualquer dúvida estou à disposição.</p>
+
+<p>At.te,</p>
+<p>David Medeiros</p>
+'''
+
+mail.Send()
+
+print('E-mail enviado')
